474f3802e0b7fd365d23e3a148ef41591e9838ab
OtinD <OtinD@outlook.com>
2018-03-13 19:53:13 +0800

    Leetcode认了 但num2node中出现bug

--- 02/main.py
+++ 02/main.py
@@ -6,17 +6,15 @@ class ListNode(object):
 	def __init__(self, x):
      		self.val = x
      		self.next = None
-     	def getnum(self):
-     		mlist = [self.val]
-     		A = self.next
+
+class Solution(object):
+	def getnum(self, mNode):
+     		mlist = [mNode.val]
+     		A = mNode.next
      		while A != None:
      			mlist.insert(0, A.val)
      			A = A.next
-#     		print mlist
      		return reduce(lambda x, y: x*10 + y, mlist)
-#    		print number
-
-class Solution(object):
 	def num2node(self, num):
 		'Transform number to ListNode'
 		mNode = ListNode(num % 10)
@@ -31,7 +29,7 @@ class Solution(object):
 		return mNode
     	def addTwoNumbers(self, l1, l2):
     		'You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order and each of their nodes contain a single digit. Add the two numbers and return it as a linked list.'
-    		return self.num2node(l1.getnum() + l2.getnum())
+    		return self.num2node(self.getnum(l1) + self.getnum(l2))
 
 l1 = ListNode(2)
 l1.next = ListNode(4)
@@ -52,4 +50,6 @@ l2.next.next = ListNode(4)
 #	A = A.next
 #	a = a / 10
 
-print Solution().addTwoNumbers(l1, l2).getnum()
+print Solution().getnum(Solution().addTwoNumbers(l1, l2))
+
+print Solution().num2node(0).next.next
