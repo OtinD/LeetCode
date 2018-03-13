9db54be4d1353e9aacb32f549fd0d1d12c650e99
OtinD <OtinD@outlook.com>
2018-03-13 20:16:41 +0800

    将tab替换成四个空格 就不会出现编译错误了

--- 02/main.py
+++ 02/main.py
@@ -3,31 +3,31 @@
 
 'Definition for singly-linked list.'
 class ListNode(object):
-	def __init__(self, x):
-     		self.val = x
-     		self.next = None
+    def __init__(self, x):
+        self.val = x
+        self.next = None
 
 class Solution(object):
-	def getnum(self, mNode):
-     		mlist = [mNode.val]
-     		A = mNode.next
-     		while A != None:
-     			mlist.insert(0, A.val)
-     			A = A.next
-     		return reduce(lambda x, y: x*10 + y, mlist)
-	def num2node(self, num):
-		'Transform number to ListNode'
-		A = ListNode(num % 10)
-		mNode = A
-		num = num / 10
-		while num != 0:
-			A.next = ListNode(num % 10)
-			A = A.next
-			num = num / 10
-		return mNode
-    	def addTwoNumbers(self, l1, l2):
-    		'You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order and each of their nodes contain a single digit. Add the two numbers and return it as a linked list.'
-    		return self.num2node(self.getnum(l1) + self.getnum(l2))
+    def getnum(self, mNode):
+        mlist = [mNode.val]
+        A = mNode.next
+        while A != None:
+            mlist.insert(0, A.val)
+            A = A.next
+        return reduce(lambda x, y: x*10 + y, mlist)
+    def num2node(self, num):
+        'Transform number to ListNode'
+        A = ListNode(num % 10)
+        mNode = A
+        num = num / 10
+        while num != 0:
+            A.next = ListNode(num % 10)
+            A = A.next
+            num = num / 10
+        return mNode
+    def addTwoNumbers(self, l1, l2):
+        'You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order and each of their nodes contain a single digit. Add the two numbers and return it as a linked list.'
+        return self.num2node(self.getnum(l1) + self.getnum(l2))
 
 l1 = ListNode(2)
 l1.next = ListNode(4)
