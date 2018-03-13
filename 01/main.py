39635181c33e03a59d511cf16253874883992bbb
OtinD <OtinD@outlook.com>
2018-03-13 17:33:31 +0800

    利用dict实现空间换时间（哈希） 注意zip()可以将两个list组合成dict

--- 01/main.py
+++ 01/main.py
@@ -15,23 +15,31 @@ class Solution(object):
      		:type target: int
      		:rtype: List[int]
      		"""
-        	list_len = len(nums)
-		flag = 0
-		for i1 in range(0, list_len - 1):
-			for i2 in range(i1 + 1, list_len):
-				if nums[i1] + nums[i2] == target:
-					flag = True
-					break
-			if flag:
-				break
-			i1 = i1 + 1
-		if flag:
-			return (i1, i2)
-		else:
-			print "no exist"
+     		list_len = len(nums)
+     		value = range(0, list_len)
+     		dictionary = dict(zip(nums, value))
+     		for i in range(0, list_len):
+     			compliment = target - nums[i]
+     			if compliment in dictionary and dictionary[compliment] != i:
+     				return (i, dictionary[compliment])
+     		print "no exist"
+#        	list_len = len(nums)
+#		flag = 0
+#		for i1 in range(0, list_len - 1):
+#			for i2 in range(i1 + 1, list_len):
+#				if nums[i1] + nums[i2] == target:
+#					flag = True
+#					break
+#			if flag:
+#				break
+#			i1 = i1 + 1
+#		if flag:
+#			return (i1, i2)
+#		else:
+#			print "no exist"
 
 nums = [2, 7, 23, 4]
-target = 32
+target = 6
 a = Solution()
 print a.twoSum(nums, target)
 
