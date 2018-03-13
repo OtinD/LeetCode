d8711b544555dca9c169dc8f2bf2f39e6b640880
OtinD <OtinD@outlook.com>
2018-03-13 16:45:28 +0800

    第一题，最蠢的方法

---
+++ 01/main.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+# Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+# 
+# You may assume that each input would have exactly one olution, and you may not use the same element twice.# 
+
+class Solution(object):
+#	def __init__(selt):
+#		pass
+	def twoSum(self, nums, target):
+		'Given an array of integers, return indices of the two numbers such that they add up to a specific target.\nYou may assume that each input would have exactly one olution, and you may not use the same element twice.'
+     		"""
+     		:type nums: List[int]
+     		:type target: int
+     		:rtype: List[int]
+     		"""
+        	list_len = len(nums)
+		flag = 0
+		for i1 in range(0, list_len - 1):
+			for i2 in range(i1 + 1, list_len):
+				if nums[i1] + nums[i2] == target:
+					flag = True
+					break
+			if flag:
+				break
+			i1 = i1 + 1
+		if flag:
+			return (i1, i2)
+		else:
+			print "no exist"
+
+nums = [2, 7, 23, 4]
+target = 32
+a = Solution()
+print a.twoSum(nums, target)
+
